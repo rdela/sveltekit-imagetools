b0b65eb171696404dd3940a4037eff3dd9263238
Ricky de Laveaga <git@rdela.com>
2022-10-20 16:11:55 -0700

    sprinkle lightly with social and seo fairy dust

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,35 @@
 <script>
+	import { page } from '$app/stores';
 	import Image from '$lib/Image.svelte';
 	// when we import static assets like images, Vite provides us with a hashed
 	// URL that can be cached forever so uses only have to download it once
 	import carinaNircam from '$lib/main_image_star-forming_region_carina_nircam_final-5mb.jpg?w=2048&invert';
+	const carinaNircamAlt =
+		'an undulating, translucent star-forming region in the Carina Nebula is shown in this Webb image, hued in ambers and blues; foreground stars with diffraction spikes can be seen, as can a speckling of background points of light through the cloudy nebula';
+	const title = 'SvelteKit Imagetools example';
+	const description =
+		'A basic configuration for SvelteKit with Imagetools set up for image processing with sharp';
 </script>
 
-<h1>SvelteKit Imagetools example</h1>
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@sveltejs" />
+	<meta name="twitter:creator" content="@rickydelaveaga" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta
+		name="twitter:image"
+		content="{$page.url.origin}{carinaNircam.fallback.src}"
+	/>
+	<meta name="twitter:image:alt" content={carinaNircamAlt} />
+</svelte:head>
+
+<h1>{title}</h1>
 
 <figure>
-	<Image
-		src={carinaNircam}
-		alt="an undulating, translucent star-forming region in the Carina Nebula is shown in this Webb image, hued in ambers and blues; foreground stars with diffraction spikes can be seen, as can a speckling of background points of light through the cloudy nebula"
-	/>
+	<Image src={carinaNircam} alt={carinaNircamAlt} />
 	<figcaption>
 		Image: <a
 			href="https://www.nasa.gov/image-feature/goddard/2022/nasa-s-webb-reveals-cosmic-cliffs-glittering-landscape-of-star-birth"
