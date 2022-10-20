7ecc0a6e342937a0feb3d45d8b1856b752720212
Ricky de Laveaga <git@rdela.com>
2022-10-20 16:50:02 -0700

    configure for vercel edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 import importAssets from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ edge: true })
 	},
 	preprocess: [
 		importAssets({
